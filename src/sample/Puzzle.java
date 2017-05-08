1a34439253fa15ef1ba4329cfb26bc0d90658d10
Tomasz Macisowicz <tmacisowicz@gmail.com>
2017-05-08 09:36:19 -1100

    add new files

---
+++ src/sample/Puzzle.java
@@ -0,0 +1,17 @@
+package sample;
+
+import javafx.animation.Timeline;
+import javafx.application.Application;
+
+public class Puzzle extends Application {
+
+
+private Timeline timeline;
+
+private void
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
